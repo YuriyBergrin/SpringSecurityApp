ebb80057f0c73e03c4b7528b4b9d10c30f66623d
yuriy bergrin <yuriy.bergrin@softswiss.com>
2024-11-19 13:43:58 +0700

    update

--- SpringSecurytyApp/src/main/java/com/gmail/bergrin/SpringSecurytyApp/config/SecurityConfig.java
+++ SpringSecurytyApp/src/main/java/com/gmail/bergrin/SpringSecurytyApp/config/SecurityConfig.java
@@ -4,9 +4,11 @@ import com.gmail.bergrin.SpringSecurytyApp.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -23,18 +25,38 @@ public class SecurityConfig {
   }
 
   @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http
-        .authorizeHttpRequests(authorize -> authorize
+  public PasswordEncoder passwordEncoder() {
+    // Замените NoOpPasswordEncoder.getInstance() на вашу реализацию PasswordEncoder
+    return NoOpPasswordEncoder.getInstance();
+  }
+
+
+  @Bean
+  public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    // Configure AuthenticationManagerBuilder
+    AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+    authenticationManagerBuilder.userDetailsService(personDetailsService);  ///*authenticationProvider(authProvider)*/
+    // Get AuthenticationManager
+    AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+
+    http.csrf(AbstractHttpConfigurer::disable);
+
+    http.authenticationManager(authenticationManager)
+        .authorizeHttpRequests((authz) -> authz
+            .requestMatchers("/", "/auth/**").permitAll()
             .anyRequest().authenticated()
-        )
-        .formLogin(Customizer.withDefaults())
-        .userDetailsService(personDetailsService);
+        );
+
+    http.formLogin((formLogin) ->
+        formLogin
+            .loginPage("/auth/login")
+            .loginProcessingUrl("/process_login")
+            .defaultSuccessUrl("/hello", true)
+            .failureUrl("/auth/login?error")
+    );
+
     return http.build();
   }
 
-  @Bean
-  public PasswordEncoder getPasswordEncoder() {
-    return NoOpPasswordEncoder.getInstance();
-  }
+
 }
